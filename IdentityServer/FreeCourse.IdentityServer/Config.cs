eb18cd25c2165400f36c425d2ae135e875e5d7c5
sibelkrblt <sibel.karabulut.20@gmail.com>
2024-05-28 19:03:32 +0300

    Microservice Basket.API hazırlandı.

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -15,6 +15,7 @@ namespace FreeCourse.IdentityServer
 			{
 				new ApiResource("resource_catalog"){Scopes={ "catalog_fullpermission" }},
 				new ApiResource("resource_photo_stock"){Scopes={ "photo_stock_fullpermission" }},
+				new ApiResource("resource_basket"){Scopes={ "basket_fullpermission" }},
 				new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
 			};
 		public static IEnumerable<IdentityResource> IdentityResources =>
@@ -31,6 +32,7 @@ namespace FreeCourse.IdentityServer
 			{
 				new ApiScope("catalog_fullpermission","Catalog API için full erişim"),
 				new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
+				new ApiScope("basket_fullpermission","Basket API için full erişim"),
 				new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
 			};
 
@@ -53,7 +55,7 @@ namespace FreeCourse.IdentityServer
 					AllowOfflineAccess=true,
 					ClientSecrets = {new Secret("secret".Sha256())},
 					AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-					AllowedScopes ={IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName,"roles" },
+					AllowedScopes ={ "basket_fullpermission", IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName,"roles" },
 					AccessTokenLifetime=1*60*60,
 					RefreshTokenExpiration=TokenExpiration.Absolute,
 					AbsoluteRefreshTokenLifetime=(int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
