8f40a697b588f2b89b096ec17b3e2e5eb3d51c6e
sibelkrblt <sibel.karabulut.20@gmail.com>
2024-06-01 16:41:04 +0300

    Microservice Order.API Onion Architecture ve Domain Driven Design yapısıyla Mssql veritabanı kullanılarak projeye eklendi. CQRS pattern MediatR kütüphanesi kullanılarak projede kullanıldı. Endpoitleri yazıldı. Postman'de gerekli testleri yapıldı. Microservice FakePayment.API projeye eklendi. IdentityServer konfigürasyonu yapıldı. Postman'de gerekli testleri yapıldı.

---
+++ Services/Order/FreeCourse.Services.Order.API/Program.cs
@@ -0,0 +1,60 @@
+using FreeCourse.Services.Order.Application.Handlers;
+using FreeCourse.Services.Order.Infrastructure;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+JsonWebTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+	options.Authority = builder.Configuration["IdentityServerURL"];
+	options.Audience = "resource_order";
+	options.RequireHttpsMetadata = false;
+});
+
+builder.Services.AddDbContext<OrderDbContext>(opt => 
+{
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), configure => 
+	{
+		configure.MigrationsAssembly("FreeCourse.Services.Order.Infrastructure");
+	});
+});
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<GetOrdersByUserIdQueryHandler>());
+
+builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+
+builder.Services.AddControllers(opt => 
+{
+	opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
